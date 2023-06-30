d4f11b323935ba6ee38fd5980939c82694561a20
Yuri <yuri@MacBook-Air-Yuri.local>
2023-06-30 19:43:26 +0200

    feat: mapper for subject fix givenName

--- src/main/java/com/example/demo/mapper/SubjectMapper.java
+++ src/main/java/com/example/demo/mapper/SubjectMapper.java
@@ -4,10 +4,12 @@ import com.example.demo.api.CreateSubjectRequest;
 import com.example.demo.api.SubjectResponse;
 import com.example.demo.domain.Subject;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface SubjectMapper {
 
+  @Mapping(target = "lastName", source = "givenName")
   Subject map(CreateSubjectRequest request);
 
   SubjectResponse map(Subject subject);
--- src/main/java/com/example/demo/resource/SubjectResource.java
+++ src/main/java/com/example/demo/resource/SubjectResource.java
@@ -20,17 +20,14 @@ public class SubjectResource {
 
   @PostMapping("/subjects")
   public ResponseEntity<Void> create(@RequestBody CreateSubjectRequest request) {
-    return ResponseEntity.created(URI.create("http://localhost:8080/subjects/" + subjectService.save(request))).build();
+    return ResponseEntity.created(URI.create("http://localhost:8080/" + subjectService.save(request))).build();
   }
 
   @GetMapping("/{id}")
   public ResponseEntity<SubjectResponse> findById(@PathVariable Long id) {
     final var result = subjectService.findById(id);
 
-    if (result.isEmpty()) {
-      return ResponseEntity.notFound().build();
-    } else {
-      return ResponseEntity.ok(result.get());
-    }
+    if (result.isEmpty()) return ResponseEntity.notFound().build();
+    return ResponseEntity.ok(result.get());
   }
 }
