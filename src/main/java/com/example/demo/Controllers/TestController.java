9da5bb3acd89c48fe504b7311ae2172a7b71a542
Yuri <yuri@MacBook-Air-Yuri.local>
2023-06-30 16:51:53 +0200

    feat: test controllers

--- src/main/java/com/example/demo/Controllers/TestController.java
+++ src/main/java/com/example/demo/Controllers/TestController.java
@@ -1,12 +1,41 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.domain.BankAccount;
+import com.example.demo.domain.Subject;
+import com.example.demo.repository.BankAccountRepository;
+import com.example.demo.repository.SubjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.transaction.Transactional;
+
 @RestController
 public class TestController {
+    @Autowired(required = false)
+    private SubjectRepository subjectRepository;
+
+    @Autowired(required = false)
+    private BankAccountRepository bankAccountRepository;
+
     @GetMapping("/test")
     public String test () {
         return "test mess";
+    }
+
+    @GetMapping("/createBank")
+    @Transactional
+    public String createBank () {
+        BankAccount bankAccount = new BankAccount();
+        bankAccountRepository.save(bankAccount);
+        return "createBank";
+    }
+
+    @GetMapping("/createAccount")
+    @Transactional
+    public String createAccount () {
+        Subject subject = new Subject();
+        subjectRepository.save(subject);
+        return "createAccount";
     }
 }
